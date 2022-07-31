2a26b8e8db76926c87f7f79ac9948d94e66c627c
varjiliyas <79210716+varjiliyas@users.noreply.github.com>
2022-07-31 23:36:08 +0530

    initialize repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player';
+import { useStateProviderValue } from "./StateProvider"
+
+const spotify = new SpotifyWebApi()
 
 function App() {
+  const [{ token}, dispatch] = useStateProviderValue();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      spotify.setAccessToken(_token);
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token
+      });
+
+      spotify.getPlaylist('37i9dQZF1E34Ucml4HHx1w').then(Response=>
+        dispatch({
+          type:"SET_DISCOVER_WEEKLY",
+          discover_weekly: Response,
+        })
+         ) 
+
+         spotify.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: spotify,
+      });
+
+      spotify.getMe().then((user) =>{
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
+      }
+      spotify.getUserPlaylists().then((plaaylists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          plaaylists: plaaylists,
+        });
+      });
+  },[token, dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!token && <Login/>}
+       {token && <Player spotify={spotify} />}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import SearchIcon from '@material-ui/icons/Search';
+import {Avatar} from "@material-ui/core"
+import React from 'react'
+import "./Header.css";
+import { useStateProviderValue } from "./StateProvider";
+function Header() {
+    const [{user}, dispatch] = useStateProviderValue();
+    return (
+        <div className="header">
+          <div className="header__left">
+            <SearchIcon />
+            <input
+              placeholder="Search for Artists, Songs, or Podcasts "
+              type="text"
+            />
+          </div>
+          <div className="header__right">          
+            {/* <Avatar alt={user?.display_name} src={user?.images[0].url} /> */}
+            <h4>{user?.display_name}</h4>
+          </div>
+        </div>
+  )
+}
+
+export default Header;
